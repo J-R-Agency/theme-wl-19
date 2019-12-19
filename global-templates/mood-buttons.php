5b241082d3d01247f18d2362a9ff44094d5bc41b
Greg Macoy <greg.macoy@jnragency.co.uk>
2019-12-19 17:37:50 +0000

    Tweak 'mood' colours and fix rogue greater than symbol

--- global-templates/mood-buttons.php
+++ global-templates/mood-buttons.php
@@ -3,15 +3,15 @@
 			<button class="btn btn--active">Active</button>
 		</div>
 		<div class="col-md-2 col-4">
-			<button class="btn btn--creative">>Creative</button>
+			<button class="btn btn--creative">Creative</button>
 		</div>
 		<div class="col-md-2 col-4">
-			<button class="btn btn--useful">>Useful</button>
+			<button class="btn btn--useful">Useful</button>
 		</div>
 		<div class="col-md-2 col-4">
-			<button class="btn btn--social">>Social</button>
+			<button class="btn btn--social">Social</button>
 		</div>
 		<div class="col-md-2 col-4">
-			<button class="btn btn--calm">>Calm</button>
+			<button class="btn btn--calm">Calm</button>
 		</div>
 	</div>
