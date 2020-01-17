9ca13f28b6af9c8c4e13e224c60661e108ac369b
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-01-17 15:50:08 +0000

    Add activities post type and theme taxonomy

--- functions.php
+++ functions.php
@@ -64,3 +64,49 @@ function create_post_type() {
 }
 // Hooking up our function to theme setup
 add_action( 'init', 'create_post_type' );
+
+//hook into the init action and call create_topics_nonhierarchical_taxonomy when it fires
+ 
+add_action( 'init', 'create_topics_nonhierarchical_taxonomy', 0 );
+ 
+function create_topics_nonhierarchical_taxonomy() {
+ 
+// Labels part for the GUI
+ 
+  $labels = array(
+    'name' => _x( 'Themes', 'taxonomy general name' ),
+    'singular_name' => _x( 'Theme', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Themes' ),
+    'popular_items' => __( 'Popular Themes' ),
+    'all_items' => __( 'All Themes' ),
+    'parent_item' => null,
+    'parent_item_colon' => null,
+    'edit_item' => __( 'Edit Theme' ), 
+    'update_item' => __( 'Update Theme' ),
+    'add_new_item' => __( 'Add New Theme' ),
+    'new_item_name' => __( 'New Topic Theme' ),
+    'separate_items_with_commas' => __( 'Separate themes with commas' ),
+    'add_or_remove_items' => __( 'Add or remove themes' ),
+    'choose_from_most_used' => __( 'Choose from the most used themes' ),
+    'menu_name' => __( 'Themes' ),
+  ); 
+ 
+// Now register the non-hierarchical taxonomy like tag
+ 
+  register_taxonomy('themes','post',array(
+    'hierarchical' => false,
+    'labels' => $labels,
+    'show_ui' => true,
+    'show_admin_column' => true,
+    'update_count_callback' => '_update_post_term_count',
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'theme' ),
+  ));
+}
+
+
+
+
+
+
+
