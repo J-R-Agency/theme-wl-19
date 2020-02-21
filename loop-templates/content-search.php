fcc27cec4b137cdfe647001045aa45e479e92f1a
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-02-21 15:04:32 +0000

    Search summary display logic

--- loop-templates/content-search.php
+++ loop-templates/content-search.php
@@ -48,9 +48,6 @@ defined( 'ABSPATH' ) || exit;
 		$search_summary_parts_max = 3 ;
 
 		while ( $i <= $search_summary_parts_max ) {
-
-			//echo " ( " . strlen( $search_summary_display . " ) " ;
-
 			
 			if ( strlen( $search_summary_display >= $search_summary_display_max ) ) {
 				break ;
