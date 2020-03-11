e3e2ba6327acc7254c43c02a52446ef438ec3691
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-03-11 14:49:17 +0000

    Adding expose wishlist functions

--- global-templates/right-sidebar-check.php
+++ global-templates/right-sidebar-check.php
@@ -17,6 +17,16 @@ defined( 'ABSPATH' ) || exit;
 
 	<?php get_template_part( 'sidebar-templates/sidebar', 'right' ); ?>
 
-	<?php print_r ( get_user_favorites_list($user_id, $site_id, $include_links, $filters, $include_button, $include_thumbnails = false, $thumbnail_size = 'thumbnail', $include_excerpt = false) ); ?>
+	<?php 
+		// Get function (returns array of IDs): 
+		print_r ( get_user_favorites($user_id, $site_id) );
+
+		// Get function (returns html list): 
+		print_r ( get_user_favorites_list($user_id, $site_id, $include_links, $filters, $include_button, $include_thumbnails = false, $thumbnail_size = 'thumbnail', $include_excerpt = false) ) ;
+
+		// Print function (prints an html list): 
+		the_user_favorites_list($user_id, $site_id, $include_links, $filters, $include_button, $include_thumbnails = false, $thumbnail_size = 'thumbnail', $include_excerpt = false) ;
+
+	?>
 
 <?php endif;
