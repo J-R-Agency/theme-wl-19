22d62ce39e30f2ae1c197d9e75445cd8cd57a8a5
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-03-26 11:42:55 +0000

    Adding day taxonomy

--- functions.php
+++ functions.php
@@ -149,6 +149,49 @@ function create_costs_nonhierarchical_taxonomy() {
 
 
 
+
+
+//hook into the init action and call create_days_nonhierarchical_taxonomy when it fires
+ 
+add_action( 'init', 'create_days_nonhierarchical_taxonomy', 0 );
+ 
+function create_days_nonhierarchical_taxonomy() {
+ 
+// Labels part for the GUI
+ 
+  $labels = array(
+    'name' => _x( 'Days', 'taxonomy general name' ),
+    'singular_name' => _x( 'Day', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Days' ),
+    'popular_items' => __( 'Popular Days' ),
+    'all_items' => __( 'All Days' ),
+    'parent_item' => null,
+    'parent_item_colon' => null,
+    'edit_item' => __( 'Edit Day' ), 
+    'update_item' => __( 'Update Day' ),
+    'add_new_item' => __( 'Add New Day' ),
+    'new_item_name' => __( 'New Topic Day' ),
+    'separate_items_with_commas' => __( 'Separate days with commas' ),
+    'add_or_remove_items' => __( 'Add or remove days' ),
+    'choose_from_most_used' => __( 'Choose from the most used days' ),
+    'menu_name' => __( 'Days' ),
+  ); 
+ 
+// Now register the non-hierarchical taxonomy like tag
+ 
+  register_taxonomy('days','activities',array(
+    'hierarchical' => false,
+    'labels' => $labels,
+    'show_ui' => true,
+    'show_admin_column' => true,
+    'update_count_callback' => '_update_post_term_count',
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'day' ),
+  ));
+}
+
+
+
 // Limit search to custom post type
 
 function searchfilter($query) {
