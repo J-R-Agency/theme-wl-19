595c3ee1c78d7a881db52c12d20d86e0bef2fc72
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-03-27 13:48:23 +0000

    Add API tax to search results

--- loop-templates/content-search.php
+++ loop-templates/content-search.php
@@ -68,6 +68,11 @@ defined( 'ABSPATH' ) || exit;
 		}
 		echo $search_summary_display;
 		?>
+			<div class="activity-taxonomies">
+				<div class="activity-taxonomies__theme"><?php the_terms( $post->ID, 'themes', 'Themes: ', '  ' ); ?></div>
+				<div class="activity-taxonomies__cost"><?php the_terms( $post->ID, 'costs', ' Cost: ', '  ' ); ?></div>
+				<div class="activity-taxonomies__days"><?php the_terms( $post->ID, 'days', ' Days: ', '  ' ); ?></div>
+			</div>		
 		<?php echo "<div class=\"wishlist\"> " . get_favorites_button($post_id, $site_id) . "</div>"; ?>
 
 	</div><!-- .entry-summary -->
