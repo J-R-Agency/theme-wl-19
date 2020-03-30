4eb2c4c1f5429e922e5e12b11d17d0d977229dd8
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-03-30 09:18:04 +0100

    Altering background hero image style for content pages

--- page-templates/activity-page.php
+++ page-templates/activity-page.php
@@ -17,7 +17,9 @@ get_header(); ?>
 	
 	<?php if ( has_post_thumbnail() ): ?>
 		<?php $thumb = get_the_post_thumbnail_url(); ?>
-			<div class="hero" style="background-image: url('<?php echo $thumb; ?>') ;"></div>	
+			<div class="hero" style="background-image: url('<?php echo $thumb; ?>') ;">
+				<div class="hero_inner" style="background-image: url('<?php echo $thumb; ?>') ;"></div>	
+			</div>	
 	<?php endif ?>
 	
 	
