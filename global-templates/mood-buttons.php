8be3bcff43a991f3f4d3733a6a48dfd00777fdcb
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-03-30 09:02:23 +0100

    Reorder themes to alphabetical

--- global-templates/mood-buttons.php
+++ global-templates/mood-buttons.php
@@ -3,15 +3,15 @@
 			<a href="/active/" title="Be active"><button class="btn btn--active">Active</button></a>
 		</div>
 		<div class="col-md-2 col-4">
-			<a href="/creative/" title="Be creative"><button class="btn btn--creative">Creative</button></a>
+			<a href="/calm/" title="Be calm"><button class="btn btn--calm">Calm</button></a>
 		</div>
 		<div class="col-md-2 col-4">
-			<a href="/useful/" title="Be useful"><button class="btn btn--useful">Useful</button></a>
+			<a href="/creative/" title="Be creative"><button class="btn btn--creative">Creative</button></a>
 		</div>
 		<div class="col-md-2 col-4">
 			<a href="/social/" title="Be social"><button class="btn btn--social">Social</button></a>
 		</div>
 		<div class="col-md-2 col-4">
-			<a href="/calm/" title="Be calm"><button class="btn btn--calm">Calm</button></a>
+			<a href="/useful/" title="Be useful"><button class="btn btn--useful">Useful</button></a>
 		</div>
 	</div>
