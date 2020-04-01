7af8487915f7699d20f91137592fa618913cde75
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-01 11:21:52 +0100

    Blog landing page test

---
+++ page-templates/blog.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Template Name: Blog landing page
+ *
+ * Template for displaying a blog landing page.
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header(); ?>
+
+<div class="container">
+	
+	<div class="row mt-20">
+		
+		<!-- Lead story -->
+		
+	</div>
+	
+	<div class="row mt-20">
+
+		<!-- News & Views -->
+
+	</div>
+	
+	<div class="row mt-20">
+		
+		<!-- Reports & Publications -->
+		
+	</div>
+
+	<div class="row mt-20">
+		
+		<!-- Advice & Information -->
+		
+	</div>
+
+</div>
+
+<?php
+get_footer();
+?>
