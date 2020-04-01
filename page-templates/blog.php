8d124de145a29285d8b95b5991db7fd77dc92135
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-01 17:37:32 +0100

    adding blog cards

--- page-templates/blog.php
+++ page-templates/blog.php
@@ -52,6 +52,17 @@ get_header(); ?>
 	
 	<div class="row mt-20">
 
+<?php $catquery = new WP_Query( 'cat=38&posts_per_page=5' ); ?>
+<ul>
+<?php while($catquery->have_posts()) : $catquery->the_post(); ?>
+<li><h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+<ul><li><?php the_content(); ?></li>
+</ul>
+</li>
+<?php endwhile; ?> 
+</ul>
+<?php wp_reset_postdata(); ?>
+
 		<!-- News & Views -->
 
 	</div>
