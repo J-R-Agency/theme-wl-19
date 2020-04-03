e15d0db2028d36dede15b1d70792491f208415e8
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-03 15:00:27 +0100

    adding article styling

--- loop-templates/content-search.php
+++ loop-templates/content-search.php
@@ -97,24 +97,7 @@ foreach ($categories as $category){
 
 }
 
-//print_r(sort($days));
 
-
-function cmp($a, $b)
-{
-    if ($a == $b) {
-        return 0;
-    }
-    return ($a < $b) ? -1 : 1;
-}
-
-$a = array(3, 2, 5, 6, 1);
-
-usort($a, "cmp");
-
-foreach ($a as $key => $value) {
-   // echo "$key: $value\n";
-}
 
 		?>
 			<div class="activity-taxonomies">
