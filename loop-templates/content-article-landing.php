b9b0f6c91b98f84f4ac1872a6c9b8f49806f1ba0
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-03 09:52:46 +0100

    test content page

--- loop-templates/content-article-landing.php
+++ loop-templates/content-article-landing.php
@@ -67,59 +67,7 @@ defined( 'ABSPATH' ) || exit;
 		<?php wp_reset_postdata(); ?>
 		</div>
 	</div>
-
-<?php
-
-// check if the repeater field has rows of data
-if( have_rows('blog_block') ):
-
- 	// loop through the rows of data
-    while ( have_rows('blog_block') ) : the_row();
-
-        // display a sub field value
-        $block_title = the_sub_field('block_title');
-        $block_intro = the_sub_field('block_intro');
-        $block_category = the_sub_field('block_category');
-/*
-        echo "<pre>" ;
-        print_r( $block_title );
-        print_r( $block_intro );
-        print_r( $block_category );
-        echo "</pre>" ;
-*/
-
-
-?>
-	<div class="row mt-20">
-		<div class="blog_container flex-container">
-		<?php $catquery = new WP_Query( 'category_name=reports&posts_per_page=3' ); ?>
-		<?php while($catquery->have_posts()) : $catquery->the_post(); ?>
-			<div class="blog-item flex-item">
-				<div class="blog-item__img">
-					<img src="">
-					<?php get_the_post_thumbnail(); ?>
-				</div>
-				<h3 class="blog-item__title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-				<div class="blog-item__summary">
-					<?php the_excerpt(); ?>
-				</div>
-			</div>
-		<?php endwhile; ?> 
-		<?php wp_reset_postdata(); ?>
-		</div>
-	</div>
-<?
-
-    endwhile;
-
-else :
-
-    // no rows found
-
-endif;
-
-?>
-
+	
 
 		<?php
 		wp_link_pages(
