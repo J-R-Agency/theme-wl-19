bcfcfaf71d37fc4a33b0d38a1010a4654c7b6d11
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-03 13:05:57 +0100

    adding article styling

--- loop-templates/content-article-landing.php
+++ loop-templates/content-article-landing.php
@@ -35,7 +35,7 @@ defined( 'ABSPATH' ) || exit;
 				<div class=\"lead_story__details flex-item\">
 					<h3><a href=\"\">" . $post->post_title . "</a></h3>
 					<p class=\"lead_story__excerpt\">" . $post->post_excerpt . "</p>
-					<div class=\"lead_story__button\"><a href=\"#\"> Read more &gt;</a></div>
+					<div class=\"lead_story__button\"><a href=\"" . the_permalink() . "\"> Read more &gt;</a></div>
 				</div>
 			</div>";
 		}
