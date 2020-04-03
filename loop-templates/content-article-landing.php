ffd353db766729ba87390b4937f8787b948ad591
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-03 09:33:58 +0100

    test content page

--- loop-templates/content-article-landing.php
+++ loop-templates/content-article-landing.php
@@ -21,7 +21,33 @@ defined( 'ABSPATH' ) || exit;
 
 	<div class="entry-content">
 
-		<?php the_content(); ?>HERE
+		<?php the_content(); ?>
+
+
+
+				<!-- Lead story -->
+		<?php
+		$lead_story = get_field( "lead_story" );
+		if (  $lead_story != "" ){
+			global $post;
+			$post = $lead_story ;
+			setup_postdata( $post );
+			//print_r( $post );
+			echo "
+			<div class=\"lead_story__card flex-container\">
+				<div class=\"lead_story__image flex-item\">
+					<img src=\"\"> 
+				</div>
+				<div class=\"lead_story__details flex-item\">
+					<h3><a href=\"\">" . $post->post_title . "</a></h3>
+					<p class=\"lead_story__excerpt\">" . $post->post_excerpt . "</p>
+					<div class=\"lead_story__button\"><a href=\"#\"> Read more &gt;</a></div>
+				</div>
+			</div>";
+		}
+
+		?>
+		
 
 		<?php
 		wp_link_pages(
