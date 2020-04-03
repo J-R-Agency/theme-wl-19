6417e04de07b783698eb859a0593f740325d8f80
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-03 09:39:56 +0100

    test content page

--- loop-templates/content-article-landing.php
+++ loop-templates/content-article-landing.php
@@ -49,6 +49,24 @@ defined( 'ABSPATH' ) || exit;
 
 		?>
 
+	<div class="row mt-20">
+		<div class="blog_container flex-container">
+		<?php $catquery = new WP_Query( 'category_name=reports&posts_per_page=3' ); ?>
+		<?php while($catquery->have_posts()) : $catquery->the_post(); ?>
+			<div class="blog-item flex-item">
+				<div class="blog-item__img">
+					<img src="">
+					<?php get_the_post_thumbnail(); ?>
+				</div>
+				<h3 class="blog-item__title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+				<div class="blog-item__summary">
+					<?php the_excerpt(); ?>
+				</div>
+			</div>
+		<?php endwhile; ?> 
+		<?php wp_reset_postdata(); ?>
+		</div>
+	</div>
 
 		<?php
 		wp_link_pages(
