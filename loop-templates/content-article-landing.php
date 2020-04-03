fe8eda0ecd788363b5c605217efbc0ad3bbb2217
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-03 10:15:17 +0100

    test content page

--- loop-templates/content-article-landing.php
+++ loop-templates/content-article-landing.php
@@ -87,7 +87,7 @@ if( have_rows('blog_block') ):
         print_r( $block_title );
         print_r( $block_intro );
         print_r( $block_category );
-        $current_category = get_category( $block_category ) ;
+        $current_category = get_term( $block_category, "post" ) ;
         print_r( $current_category );
         echo "</pre>" ;
 
