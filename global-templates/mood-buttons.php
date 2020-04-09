1919e39c204574ce925b405ce16cba813d04e0dc
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-09 14:02:44 +0100

    reinstating mood button links

--- global-templates/mood-buttons.php
+++ global-templates/mood-buttons.php
@@ -1,17 +1,17 @@
 	<div class="row mb-5 centered mood-btns">
 		<div class="col-md-2 col-4">
-			<a href="/theme/active/" title="Be active"><button class="btn btn--active">Active</button></a>
+			<a href="/active/" title="Be active"><button class="btn btn--active">Active</button></a>
 		</div>
 		<div class="col-md-2 col-4">
-			<a href="/theme/calm/" title="Be calm"><button class="btn btn--calm">Calm</button></a>
+			<a href="/calm/" title="Be calm"><button class="btn btn--calm">Calm</button></a>
 		</div>
 		<div class="col-md-2 col-4">
-			<a href="/theme/creative/" title="Be creative"><button class="btn btn--creative">Creative</button></a>
+			<a href="/creative/" title="Be creative"><button class="btn btn--creative">Creative</button></a>
 		</div>
 		<div class="col-md-2 col-4">
-			<a href="/theme/social/" title="Be social"><button class="btn btn--social">Social</button></a>
+			<a href="/social/" title="Be social"><button class="btn btn--social">Social</button></a>
 		</div>
 		<div class="col-md-2 col-4">
-			<a href="/theme/useful/" title="Be useful"><button class="btn btn--useful">Useful</button></a>
+			<a href="/useful/" title="Be useful"><button class="btn btn--useful">Useful</button></a>
 		</div>
 	</div>
