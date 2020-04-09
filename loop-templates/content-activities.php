eef1195bae25b03eb812c6356bd706e76098f7b9
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-09 13:39:57 +0100

    Adding support for serialisd contacts

--- loop-templates/content-activities.php
+++ loop-templates/content-activities.php
@@ -84,9 +84,6 @@ function dump($var){
 
 <?php
 
-// Address
-echo "<div class=\"main_address\">" . $wl_api_main_address . "</div>" ;
-
 if ($activity_documents != ""){
 	// Display documents
 	// Documents
@@ -145,7 +142,17 @@ if ($activity_contacts != ""){
 		$website_link = $activity_link[0] . $websiteurl . $activity_link[1] ;
 
 	}
-	echo "<div class=\"activity_contact__container\"><h3 class=\"activity_contact__title\">Contacts</h3> <ul class=\"activity_contact__list\">" . $wl_api_activity_contacts . "</ul>$website_link</div>" ;
+	echo "
+		<div class=\"activity_contact__container\">
+			<h3 class=\"activity_contact__title\">Contacts</h3>
+			<div class=\"main_address\">" . 
+			$wl_api_main_address . 
+			"</div>
+			<ul class=\"activity_contact__list\">" . 
+			$wl_api_activity_contacts . 
+			"</ul>
+			$website_link
+			</div>" ;
 }else{
 	// Do not display documents
 }
