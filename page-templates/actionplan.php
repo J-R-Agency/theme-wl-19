8db5df9f89d0d628ad0b92e86e75b3ee2c17083b
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-13 19:07:45 +0100

    Adding action plan template

---
+++ page-templates/actionplan.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Template Name: Action Plan Landing Page
+ *
+ * Template for displaying a page just with the header and footer area and a "naked" content area in between.
+ * Good for landingpages and other types of pages where you want to add a lot of custom markup.
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header(); ?>
+
+<div class="wrapper" id="single-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<!-- Do the left sidebar check -->
+			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+
+			<main class="site-main" id="main">
+				
+<form id="wl_set_goal" name="wl_set_goal" method="post" action="">
+<label for="wl_goal">Goal</label><br>
+<input type="text" id="wl_goal" name="wl_goal" tabindex="1" size="20"><br>
+<input type="submit" id="submit" name="submit" value="Set Goal">
+</form>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'loop-templates/content', 'single' ); ?>
+
+					<?php understrap_post_nav(); ?>
+
+					<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+					?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</main><!-- #main -->
+
+			<!-- Do the right sidebar check -->
+			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #single-wrapper -->
+
+<?php get_footer();
