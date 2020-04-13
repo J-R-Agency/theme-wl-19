d9a332e85d0248b6bdb4566335a8ff89d65cc979
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-13 19:13:37 +0100

    Adding action plan template

--- page-templates/actionplan.php
+++ page-templates/actionplan.php
@@ -23,7 +23,7 @@ get_header(); ?>
 			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
 			<main class="site-main" id="main">
-				ddd
+
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<?php get_template_part( 'loop-templates/content', 'actionplan' ); ?>
