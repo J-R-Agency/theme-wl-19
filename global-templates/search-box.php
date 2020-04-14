0bf70ab55cddf9fd4f908a0fffbae46bb87fe4d2
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-14 19:43:11 +0100

    Theme styling for new Search & Filter feature plugin

--- global-templates/search-box.php
+++ global-templates/search-box.php
@@ -1,6 +1,5 @@
 <div class="action-box w-100">
 	<h1>What would you like to do?</h1>
-	<?php
-	get_search_form( 'echo', 'Activity Search');
-	?>
+	<?php // get_search_form( 'echo', 'Activity Search');?>
+	<?php echo do_shortcode( '[searchandfilter fields="search,postcodes,costs,activities" submit_label="Find activities" class="find-activities"]' ); ?>
 </div>
