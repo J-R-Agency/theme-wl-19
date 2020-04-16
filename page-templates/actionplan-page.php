5ca95609674b4fa4c33185d5fa269b6ddc08c0ce
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-16 14:05:16 +0100

    Display shortlist

--- page-templates/actionplan-page.php
+++ page-templates/actionplan-page.php
@@ -229,6 +229,15 @@ if(!isset($_COOKIE[$cookie_name])) {
 
 					<!-- display shortlist from action plan (favorites plugin) -->
 					<?php
+
+
+					$cookie_name = "simplefavorites";
+					if(!isset($_COOKIE[$cookie_name])) {
+					// Not set
+					} else {
+					// Set
+					   echo $_COOKIE[$cookie_name] ;
+					}
 						the_user_favorites_list($user_id, $site_id, $include_links = true, $filters, $include_button, $include_thumbnails = false, $thumbnail_size = 'thumbnail', $include_excerpt = false) ;
 					?>
 
