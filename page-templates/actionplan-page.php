ef966142ca396bb531a0f29fe432a18c357be396
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-16 11:37:13 +0100

    Redirect to 'Action Plan' to workaround cookie setting issue

--- page-templates/actionplan-page.php
+++ page-templates/actionplan-page.php
@@ -148,65 +148,58 @@ $container = get_theme_mod( 'understrap_container_type' );
 <?php
 $cookie_name = "wl_goal";
 if(!isset($_COOKIE[$cookie_name])) {
-    echo "<br>Cookie named '" . $cookie_name . "' is not set!";
+// Not set
 } else {
-    echo "<br>Cookie '" . $cookie_name . "' is set!<br>";
-    echo "<br>Value is: " . $_COOKIE[$cookie_name];
+// Set
     $wl_goal = $_COOKIE[$cookie_name];
 }
 
 $cookie_name = "wl_step_one";
 if(!isset($_COOKIE[$cookie_name])) {
-    echo "<br>Cookie named '" . $cookie_name . "' is not set!";
+// Not set
 } else {
-    echo "<br>Cookie '" . $cookie_name . "' is set!<br>";
-    echo "<br>Value is: " . $_COOKIE[$cookie_name];
+// Set
     $wl_step_one = $_COOKIE[$cookie_name];
 }
 
 $cookie_name = "wl_step_two";
 if(!isset($_COOKIE[$cookie_name])) {
-    echo "<br>Cookie named '" . $cookie_name . "' is not set!";
+// Not set
 } else {
-    echo "<br>Cookie '" . $cookie_name . "' is set!<br>";
-    echo "<br>Value is: " . $_COOKIE[$cookie_name];
-    $wl_step_two = $_COOKIE[$cookie_name];
+// Set
+	$wl_step_two = $_COOKIE[$cookie_name];
 }
 
 $cookie_name = "wl_step_three";
 if(!isset($_COOKIE[$cookie_name])) {
-    echo "<br>Cookie named '" . $cookie_name . "' is not set!";
+// Not set
 } else {
-    echo "<br>Cookie '" . $cookie_name . "' is set!<br>";
-    echo "<br>Value is: " . $_COOKIE[$cookie_name];
+// Set
     $wl_step_three = $_COOKIE[$cookie_name];
 }
 
 $cookie_name = "wl_step_four";
 if(!isset($_COOKIE[$cookie_name])) {
-    echo "<br>Cookie named '" . $cookie_name . "' is not set!";
+// Not set
 } else {
-    echo "<br>Cookie '" . $cookie_name . "' is set!<br>";
-    echo "<br>Value is: " . $_COOKIE[$cookie_name];
+// Set
     $wl_step_four = $_COOKIE[$cookie_name];
 }
 
 $cookie_name = "wl_step_five";
 if(!isset($_COOKIE[$cookie_name])) {
-    echo "<br>Cookie named '" . $cookie_name . "' is not set!";
+// Not set
 } else {
-    echo "<br>Cookie '" . $cookie_name . "' is set!<br>";
-    echo "<br>Value is: " . $_COOKIE[$cookie_name];
+// Set
     $wl_step_five = $_COOKIE[$cookie_name];
 }
 
 $cookie_name = "wl_notes";
 if(!isset($_COOKIE[$cookie_name])) {
-    echo "<br>Cookie named '" . $cookie_name . "' is not set!";
+// Not set
 } else {
-    echo "<br>Cookie '" . $cookie_name . "' is set!<br>";
-    echo "<br>Value is: " . $_COOKIE[$cookie_name];
-    $wl_notes = $_COOKIE[$cookie_name];
+// Set
+   $wl_notes = $_COOKIE[$cookie_name];
 }
 
 ?>
