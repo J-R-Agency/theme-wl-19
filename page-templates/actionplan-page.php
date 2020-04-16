9b16271121cf3c48d71edeb5fffee6840089aab4
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-16 10:53:48 +0100

    Clearing cookies function

--- page-templates/actionplan-page.php
+++ page-templates/actionplan-page.php
@@ -22,6 +22,7 @@ if($_POST['submit']) {
 		$cookie_name = "wl_goal";
 		$cookie_value = $_POST['wl_goal'] ;
 		setcookie($cookie_name, $cookie_value, time() + apply_filters( 'simplefavorites_cookie_expiration_interval', 31556926 ), "/");
+    	echo "<br>The wl_goal exists:  " . $_COOKIE['wl_goal'] ;
 
     }
 
@@ -33,6 +34,7 @@ if($_POST['submit']) {
 		$cookie_name = "wl_step_one";
 		$cookie_value = $_POST['wl_step_one'] ;
 		setcookie($cookie_name, $cookie_value, time() + apply_filters( 'simplefavorites_cookie_expiration_interval', 31556926 ), "/");
+    	echo "<br>The wl_step_one exists:  " . $_COOKIE['wl_step_one'] ;
 
     }
 
@@ -43,6 +45,7 @@ if($_POST['submit']) {
 		$cookie_name = "wl_step_two";
 		$cookie_value = $_POST['wl_step_two'] ;
 		setcookie($cookie_name, $cookie_value, time() + apply_filters( 'simplefavorites_cookie_expiration_interval', 31556926 ), "/");
+    	echo "<br>The wl_step_two exists:  " . $_COOKIE['wl_step_two'] ;
 
     }
 
@@ -53,6 +56,7 @@ if($_POST['submit']) {
 		$cookie_name = "wl_step_three";
 		$cookie_value = $_POST['wl_step_three'] ;
 		setcookie($cookie_name, $cookie_value, time() + apply_filters( 'simplefavorites_cookie_expiration_interval', 31556926 ), "/");
+    	echo "<br>The wl_step_three exists:  " . $_COOKIE['wl_step_three'] ;
 
     }
 
@@ -63,6 +67,7 @@ if($_POST['submit']) {
 		$cookie_name = "wl_step_four";
 		$cookie_value = $_POST['wl_step_four'] ;
 		setcookie($cookie_name, $cookie_value, time() + apply_filters( 'simplefavorites_cookie_expiration_interval', 31556926 ), "/");
+    	echo "<br>The wl_step_four exists:  " . $_COOKIE['wl_step_four'] ;
 
     }
 
@@ -73,16 +78,18 @@ if($_POST['submit']) {
 		$cookie_name = "wl_step_five";
 		$cookie_value = $_POST['wl_step_five'] ;
 		setcookie($cookie_name, $cookie_value, time() + apply_filters( 'simplefavorites_cookie_expiration_interval', 31556926 ), "/");
+    	echo "<br>The wl_step_five exists:  " . $_COOKIE['wl_step_five'] ;
 
     }
 
     // Notes
     if ( $_POST['wl_notes'] ) {
     	
-    	echo "<br>The wl_notes exists:  " . $_POST['wl_notes'] ;
+    	echo "<br>The wl_notes exists:  " . $_COOKIE['wl_notes'] ;
 		$cookie_name = "wl_notes";
 		$cookie_value = $_POST['wl_notes'] ;
 		setcookie($cookie_name, $cookie_value, time() + apply_filters( 'simplefavorites_cookie_expiration_interval', 31556926 ), "/");
+    	echo "<br>The wl_notes exists:  " . $_COOKIE['wl_notes'] ;
 
     }
 
