9a630493bff820195d31bb55d88a7f05718609c3
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-17 20:32:43 +0100

    Refactor into include files (action plan logic)

--- global-templates/actionplan-email.php
+++ global-templates/actionplan-email.php
@@ -9,6 +9,15 @@ if($_POST['email']) {
     	
     	echo "<br>The wl_actionplan_actions__email exists:  " . $_POST['wl_actionplan_actions__email'] ;
 
+        $to = $_POST['wl_actionplan_actions__email']; //sendto@example.com
+        $subject = 'The subject';
+        $body = 'The email body content';
+        $headers = array('Content-Type: text/html; charset=UTF-8');
+
+        if ( wp_mail( $to, $subject, $body, $headers ) ) {
+            echo "Sent to $to "; 
+        }
+
     }
 
 	// header('Location: /action-plan/');
