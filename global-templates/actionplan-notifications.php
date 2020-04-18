f837cad2c409250d0784cd68d826f9ae679fb71f
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-18 14:27:44 +0100

    Setting notifications styles and variables

--- global-templates/actionplan-notifications.php
+++ global-templates/actionplan-notifications.php
@@ -1,12 +1,6 @@
 <?php
 // Display notifications and errors
 
-	// echo $frm_email_actionplan__output;
-	$wl_actionplan_notifications__style = "list" ;
-
-    echo wl_actionplan_notifications ( $wl_actionplan_notifications__heading, $wl_actionplan_notifications__parts, $wl_actionplan_notifications__style, $wl_actionplan_notifications__class, "" ) ;
-    echo wl_actionplan_notifications ( $wl_actionplan_notifications__heading, $wl_actionplan_notifications__parts, $wl_actionplan_notifications__style, $wl_actionplan_notifications__class, "warning" ) ;
-    echo wl_actionplan_notifications ( $wl_actionplan_notifications__heading, $wl_actionplan_notifications__parts, $wl_actionplan_notifications__style, $wl_actionplan_notifications__class, "error" ) ;
-    echo wl_actionplan_notifications ( $wl_actionplan_notifications__heading, $wl_actionplan_notifications__parts, $wl_actionplan_notifications__style, $wl_actionplan_notifications__class, "success" ) ;
+	echo $frm_email_actionplan__output;
 
 ?>
