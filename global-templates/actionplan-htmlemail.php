c2f4f6d55ba42c2695249364da7a0c0ca9367e71
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-18 18:52:23 +0100

    Get activity titles from post ids

--- global-templates/actionplan-htmlemail.php
+++ global-templates/actionplan-htmlemail.php
@@ -51,7 +51,7 @@ if( $wl_goal != ""){
       while($shortlist_posts->have_posts()) : 
          $shortlist_posts->the_post();
 
-         echo "<h3>" . get_the_title()  . " - " get_the_permalink() . "</h3>";
+         echo "<h3>" . get_the_title()  . " - " . get_the_permalink() . "</h3>";
 
         endwhile;
     else: 
