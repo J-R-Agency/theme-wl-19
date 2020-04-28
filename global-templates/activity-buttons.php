25bb115fce7e89770b4c6404a1de38185ac9dbfd
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-28 19:28:36 +0100

    Filter archive title

--- global-templates/activity-buttons.php
+++ global-templates/activity-buttons.php
@@ -7,12 +7,10 @@
 		
 		<div class="row">
 			<?php if( have_rows('activity') ): ?>
-<?php 
-$rows = get_field('activity');
-shuffle( $rows ) ; 
-?>
-				<?php while( $rows ): the_row(); 
-			
+						
+
+				<?php while( have_rows('activity') ): the_row(); 
+			 var_dump( the_row() );
 					// vars
 					$image = get_sub_field('activity_icon');
 					$name = get_sub_field('activity_name');
@@ -57,6 +55,11 @@ shuffle( $rows ) ;
 					
 				<?php endwhile; ?>
 			
+
+
+
+
+
 			<?php endif; ?>
 		</div>
 	</div>
