627d63e35e0e83406613afa7e752e826df96fd3c
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-28 21:44:24 +0100

    randomise suggested activities

--- global-templates/activity-buttons.php
+++ global-templates/activity-buttons.php
@@ -12,17 +12,12 @@
 			<?php 
 			if( have_rows('activity') ) {
 			 	while( have_rows('activity') ): the_row(); 
-			 //var_dump( the_row() );
-					// vars
 					$image = get_sub_field('activity_icon');
 					$name = get_sub_field('activity_name');
 					$description = get_sub_field('activity_description');
 					$link = get_sub_field('activity_link');
-
-
 					$suggestions[] = array( $image, $name, $description, $link);
 				endwhile; 
-
 
 				//var_dump($suggestions);
 
@@ -30,8 +25,7 @@
 
 				$shuffled_rows = array_slice($suggestions, 0, 4);
 
-
-				echo count($shuffled_rows);
+				// echo count($shuffled_rows);
 
 				foreach ($shuffled_rows as $suggested_row) {
 
@@ -82,8 +76,6 @@
 
 			}
 			?>
-
-
 
 		</div>
 	</div>
