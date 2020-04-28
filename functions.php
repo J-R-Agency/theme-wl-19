357282b65969c07549fec73a7aff4c13a7f9d915
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-28 18:38:17 +0100

    Filter archive title

--- functions.php
+++ functions.php
@@ -581,6 +581,20 @@ function random_search_result( $q ) {
 }
 add_action( 'pre_get_posts', 'random_search_result' );
 
+/* Filter Archive Title */
 
-
+add_filter( 'get_the_archive_title', function ($title) {    
+  if ( is_category() ) {    
+          $title = single_cat_title( '', false );    
+      } elseif ( is_tag() ) {    
+          $title = single_tag_title( '', false );    
+      } elseif ( is_author() ) {    
+          $title = '<span class="vcard">' . get_the_author() . '</span>' ;    
+      } elseif ( is_tax() ) { //for custom post types
+          $title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+      } elseif (is_post_type_archive()) {
+          $title = post_type_archive_title( '', false );
+      }
+  return $title;    
+});
 
