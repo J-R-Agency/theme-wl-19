59aa5bb6ac85e58be6193a8889d4dbf910aecbbf
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-28 17:22:18 +0100

    Add save steps and change copy

--- global-templates/actionplan-mainform.php
+++ global-templates/actionplan-mainform.php
@@ -22,15 +22,15 @@
 					<h2>Key steps</h2>
 					<ol>
 						<li><label class="visually-hidden" for="wl_step_one">Step 1:</label>
-<input class="wl_steps" type="text" name="wl_step_one" id="wl_step_one" value="<?php echo stripslashes( $wl_step_one );?>" placeholder="Step 1: e.g. Find out what groups there are"></li>
+<input class="wl_steps" type="text" name="wl_step_one" id="wl_step_one" value="<?php echo stripslashes( $wl_step_one );?>" placeholder=" e.g. Find out what groups there are"></li>
 						<li><label class="visually-hidden" for="wl_step_two">Step 2:</label>
-<input class="wl_steps" type="text" name="wl_step_two" id="wl_step_two" value="<?php echo stripslashes( $wl_step_two );?>" placeholder="Step 2: e.g. Join a walking group"></li>
+<input class="wl_steps" type="text" name="wl_step_two" id="wl_step_two" value="<?php echo stripslashes( $wl_step_two );?>" placeholder=" e.g. Join a walking group"></li>
 						<li><label class="visually-hidden" for="wl_step_three">Step 3:</label>
-<input class="wl_steps" type="text" name="wl_step_three" id="wl_step_three" value="<?php echo stripslashes( $wl_step_three );?>" placeholder="Step 3: e.g. Try to socialise at least once a week"></li>
+<input class="wl_steps" type="text" name="wl_step_three" id="wl_step_three" value="<?php echo stripslashes( $wl_step_three );?>" placeholder=" e.g. Try to socialise at least once a week"></li>
 						<li><label class="visually-hidden" for="wl_step_four">Step 4:</label>
-<input class="wl_steps" type="text" name="wl_step_four" id="wl_step_four" value="<?php echo stripslashes( $wl_step_four );?>" placeholder="Step 4"></li>
+<input class="wl_steps" type="text" name="wl_step_four" id="wl_step_four" value="<?php echo stripslashes( $wl_step_four );?>" placeholder="..."></li>
 						<li><label class="visually-hidden" for="wl_step_five">Step 5:</label>
-<input class="wl_steps" type="text" name="wl_step_five" id="wl_step_five" value="<?php echo stripslashes( $wl_step_five );?>" placeholder="Step 5"></li>
+<input class="wl_steps" type="text" name="wl_step_five" id="wl_step_five" value="<?php echo stripslashes( $wl_step_five );?>" placeholder="..."></li>
 					</ol>
 
 					<input type="submit" name="submit" value="Save" />
