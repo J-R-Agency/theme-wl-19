d27c7d9e75cb025e734be7d5adb23a95fa48318d
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-04-28 14:40:15 +0100

    center mood buttons

--- global-templates/mood-buttons.php
+++ global-templates/mood-buttons.php
@@ -1,17 +1,17 @@
-	<div class="row mb-5 centered mood-btns">
-		<div class="col-md-2 col-4">
+	<div class="row mb-5 centered mood-btns mood-btns__container">
+		<div class="mood-btns__item">
 			<a href="/active/" title="Be active"><button class="btn btn--active">Active</button></a>
 		</div>
-		<div class="col-md-2 col-4">
+		<div class="mood-btns__item">
 			<a href="/calm/" title="Be calm"><button class="btn btn--calm">Calm</button></a>
 		</div>
-		<div class="col-md-2 col-4">
+		<div class="mood-btns__item">
 			<a href="/creative/" title="Be creative"><button class="btn btn--creative">Creative</button></a>
 		</div>
-		<div class="col-md-2 col-4">
+		<div class="mood-btns__item">
 			<a href="/social/" title="Be social"><button class="btn btn--social">Social</button></a>
 		</div>
-		<div class="col-md-2 col-4">
+		<div class="mood-btns__item">
 			<a href="/useful/" title="Be useful"><button class="btn btn--useful">Useful</button></a>
 		</div>
 	</div>
