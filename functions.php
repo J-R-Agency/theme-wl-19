e68848a9ddb415d747a02cd979369021519d7490
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-05-07 13:25:55 +0100

    Check if ACF is active for relevant functions in functions file

--- functions.php
+++ functions.php
@@ -364,15 +364,22 @@ function wl_display_additional_information ( $args ) {
 // Aggregates all relevant AI fields pulled through from
 // the Live Well API for this particular entry 
 
-  setup_postdata ( $args ) ;
-  
-  $wl_api_additional_information = get_field("additional_information");
 
-  if ( $wl_api_additional_information != "" ) {
-    
-    echo "<div class=\"additional_information\"> " . $wl_api_additional_information . "</div>" ;
-  
+  if( ! class_exists('ACF') ) {
+    // Do nothing
+  } else {
+
+    setup_postdata ( $args ) ;
+
+    $wl_api_additional_information = get_field("additional_information");
+
+    if ( $wl_api_additional_information != "" ) {
+      
+      echo "<div class=\"additional_information\"> " . $wl_api_additional_information . "</div>" ;
+
+    }    
   }
+
 
 }
 
@@ -384,52 +391,61 @@ function wl_display_activity_contacts ( $args ) {
 // Aggregates all relevant contact fields pulled through from
 // the Live Well API for this particular entry 
 
-  setup_postdata ( $args ) ;
-  
-  $wl_api_main_address = get_field("main_address");
-  $wl_api_activity_contacts = get_field("contacts");
-  $activity_contacts = unserialize($wl_api_activity_contacts);
-
-
-  if ($activity_contacts != ""){
-
-    // Display contacts
-    // Contacts
-    foreach ($activity_contacts as $activity_contact) {
-
-      if ( $activity_contact["FullName"] != "" ){
-        $activity_contact_list[] = "<li class=\"activity_contact__item\">" . $activity_contact["FullName"] . "</li>";
-      }
-      if ( $activity_contact["EmailAddress"] != "" ){
-        $activity_contact_list[] = "<li class=\"activity_contact__item\"><a href=\"mailto:" . $activity_contact["EmailAddress"] . "\" title=\"" . $activity_contact["FullName"] . "\" target=\"_blank\">" . $activity_contact["EmailAddress"] . "</a></li>";
-      }
-      if ( $activity_contact["PhoneNumber"] != "" ){
-        $activity_contact_list[] = "<li class=\"activity_contact__item\">" . $activity_contact["PhoneNumber"] . "</li>";
-      }
-
-    }
-
-    $wl_api_activity_contacts = implode("", $activity_contact_list);
-    
-    if ( $websiteurl != "" ) {
-
-      $website_link = $activity_link[0] . $websiteurl . $activity_link[1] ;
-
-    }
-    echo "
-      <div class=\"activity_contact__container\">
-        <h3 class=\"activity_contact__title\">Contacts</h3>
-        <div class=\"main_address\">" . 
-        $wl_api_main_address . 
-        "</div>
-        <ul class=\"activity_contact__list\">" . 
-        $wl_api_activity_contacts . 
-        "</ul>
-        $website_link
-        </div>" ;
+  if( ! class_exists('ACF') ) {
+    // Do nothing
   } else {
-    // Do not display documents
+
+    setup_postdata ( $args ) ;
+    
+    $wl_api_main_address = get_field("main_address");
+    $wl_api_activity_contacts = get_field("contacts");
+    $activity_contacts = unserialize($wl_api_activity_contacts);
+
+
+    if ($activity_contacts != ""){
+
+      // Display contacts
+      // Contacts
+      foreach ($activity_contacts as $activity_contact) {
+
+        if ( $activity_contact["FullName"] != "" ){
+          $activity_contact_list[] = "<li class=\"activity_contact__item\">" . $activity_contact["FullName"] . "</li>";
+        }
+        if ( $activity_contact["EmailAddress"] != "" ){
+          $activity_contact_list[] = "<li class=\"activity_contact__item\"><a href=\"mailto:" . $activity_contact["EmailAddress"] . "\" title=\"" . $activity_contact["FullName"] . "\" target=\"_blank\">" . $activity_contact["EmailAddress"] . "</a></li>";
+        }
+        if ( $activity_contact["PhoneNumber"] != "" ){
+          $activity_contact_list[] = "<li class=\"activity_contact__item\">" . $activity_contact["PhoneNumber"] . "</li>";
+        }
+
+      }
+
+      $wl_api_activity_contacts = implode("", $activity_contact_list);
+      
+      if ( $websiteurl != "" ) {
+
+        $website_link = $activity_link[0] . $websiteurl . $activity_link[1] ;
+
+      }
+      echo "
+        <div class=\"activity_contact__container\">
+          <h3 class=\"activity_contact__title\">Contacts</h3>
+          <div class=\"main_address\">" . 
+          $wl_api_main_address . 
+          "</div>
+          <ul class=\"activity_contact__list\">" . 
+          $wl_api_activity_contacts . 
+          "</ul>
+          $website_link
+          </div>" ;
+    } else {
+      // Do not display documents
+    }
+
+    
   }
+
+
 }
 
 add_action( 'init', 'wl_display_activity_contacts', 0 );
@@ -441,23 +457,27 @@ function wl_display_activity_documents ( $args ) {
 // Aggregates all relevant contact fields pulled through from
 // the Live Well API for this particular entry 
 
-  setup_postdata ( $args ) ;
+  if( ! class_exists('ACF') ) {
+    // Do nothing
+  } else {
 
-  $wl_api_activity_documents = get_field("activity_documents");
-  $activity_documents = unserialize($wl_api_activity_documents);
+    setup_postdata ( $args ) ;
 
-  if ($activity_documents != ""){
-    // Display documents
-    // Documents
-    foreach ($activity_documents as $activity_document) {
-      $activity_document_list[] = "<li class=\"activity_document__item\"><a href=\"" . $activity_document["Url"] . "\" title=\"" . $activity_document["Title"] . "\" target=\"_blank\">" . $activity_document["Title"] . "</a></li>";
+    $wl_api_activity_documents = get_field("activity_documents");
+    $activity_documents = unserialize($wl_api_activity_documents);
+
+    if ($activity_documents != ""){
+      // Display documents
+      // Documents
+      foreach ($activity_documents as $activity_document) {
+        $activity_document_list[] = "<li class=\"activity_document__item\"><a href=\"" . $activity_document["Url"] . "\" title=\"" . $activity_document["Title"] . "\" target=\"_blank\">" . $activity_document["Title"] . "</a></li>";
+      }
+      $wl_api_activity_documents = implode("", $activity_document_list);
+      echo "<div class=\"activity_documents__container\"><h3 class=\"activity_documents__title\">Documents</h3> <ul class=\"activity_document__list\">" . $wl_api_activity_documents . "</ul></div>" ;
+    }else{
+      // Do not display documents
     }
-    $wl_api_activity_documents = implode("", $activity_document_list);
-    echo "<div class=\"activity_documents__container\"><h3 class=\"activity_documents__title\">Documents</h3> <ul class=\"activity_document__list\">" . $wl_api_activity_documents . "</ul></div>" ;
-  }else{
-    // Do not display documents
   }
-
 }
 
 add_action( 'init', 'wl_display_activity_documents', 0 );
@@ -468,33 +488,38 @@ function wl_display_activity_images ( $args ) {
 // Aggregates all relevant contact fields pulled through from
 // the Live Well API for this particular entry 
 
-  setup_postdata ( $args ) ;
 
-  $wl_api_activity_images = get_field("activity_images");
-  $activity_images = unserialize($wl_api_activity_images);
-
-
-  if ($activity_images != ""){
-    // Display images
-    // Images
-    foreach ($activity_images as $activity_image) {
-      list($width, $height, $type, $attr) = getimagesize($activity_image["Url"]);
-      // echo "$width, $height, $type, $attr";
-      unset($use_dimensions);
-      if ($width<250 && $height<250){
-        $use_dimensions = " width: " . $width . "px; height: " . $height . "px; ";
-      }else{
-        unset($use_dimensions);
-      }
-
-      $activity_image_list[] = "<a class=\"flex-item\" href=\"" . $activity_image["Url"] . "\" title=\"" . $activity_image["Description"] . "\" target=\"_blank\"><div class=\"activity_images__img\" $use_dimensions style=\"" . $use_dimensions . "background-image: url('" . $activity_image["Url"] . "')\"></div></a>" ;
-    }
-    $wl_api_activity_images = "<div class=\"flex-container\">" . implode("", $activity_image_list) . "</div>";
-    echo "<div class=\"activity_images__container\"><h3 class=\"activity_images__title\">Images</h3>" . $wl_api_activity_images . "</div>" ;
+  if( ! class_exists('ACF') ) {
+    // Do nothing
   } else {
-    // Do not display images
+
+    setup_postdata ( $args ) ;
+
+    $wl_api_activity_images = get_field("activity_images");
+    $activity_images = unserialize($wl_api_activity_images);
+
+
+    if ($activity_images != ""){
+      // Display images
+      // Images
+      foreach ($activity_images as $activity_image) {
+        list($width, $height, $type, $attr) = getimagesize($activity_image["Url"]);
+        // echo "$width, $height, $type, $attr";
+        unset($use_dimensions);
+        if ($width<250 && $height<250){
+          $use_dimensions = " width: " . $width . "px; height: " . $height . "px; ";
+        }else{
+          unset($use_dimensions);
+        }
+
+        $activity_image_list[] = "<a class=\"flex-item\" href=\"" . $activity_image["Url"] . "\" title=\"" . $activity_image["Description"] . "\" target=\"_blank\"><div class=\"activity_images__img\" $use_dimensions style=\"" . $use_dimensions . "background-image: url('" . $activity_image["Url"] . "')\"></div></a>" ;
+      }
+      $wl_api_activity_images = "<div class=\"flex-container\">" . implode("", $activity_image_list) . "</div>";
+      echo "<div class=\"activity_images__container\"><h3 class=\"activity_images__title\">Images</h3>" . $wl_api_activity_images . "</div>" ;
+    } else {
+      // Do not display images
+    }
   }
-  
 }
 
 add_action( 'init', 'wl_display_activity_images', 0 );
@@ -505,24 +530,29 @@ function wl_display_activity_largemap ( $args ) {
 // Aggregates all relevant address fields pulled through from
 // the Live Well API for this particular entry 
 
-  setup_postdata ( $args ) ;
-  
-  global $wl_google_api_key;
-
-  $wl_api_main_address = get_field("main_address");
-
-  if ( $wl_api_main_address != "" ) {
-
-    echo "
-    <iframe
-      width=\"100%\"
-      height=\"300\"
-      frameborder=\"0\" style=\"border:0\"
-      src=\"https://www.google.com/maps/embed/v1/place?key=" . $wl_google_api_key . "&q=" . urlencode ( $wl_api_main_address ) . "\" allowfullscreen>
-    </iframe>
-    ";
+  if( ! class_exists('ACF') ) {
+    // Do nothing
   } else {
+
+    setup_postdata ( $args ) ;
     
+    global $wl_google_api_key;
+
+    $wl_api_main_address = get_field("main_address");
+
+    if ( $wl_api_main_address != "" ) {
+
+      echo "
+      <iframe
+        width=\"100%\"
+        height=\"300\"
+        frameborder=\"0\" style=\"border:0\"
+        src=\"https://www.google.com/maps/embed/v1/place?key=" . $wl_google_api_key . "&q=" . urlencode ( $wl_api_main_address ) . "\" allowfullscreen>
+      </iframe>
+      ";
+    } else {
+      
+    }
   }
 }
 
@@ -534,31 +564,35 @@ function wl_display_activity_logo ( $args ) {
 // Aggregates all relevant contact fields pulled through from
 // the Live Well API for this particular entry 
 
-  setup_postdata ( $args ) ;
-  
-  // Get website URL 
-  $websiteurl = get_field('websiteurl');
-
-  // Get logo if available
-  $wl_api_logo_description = get_field("logo_description");
-  $wl_api_logo_url = get_field("logo_url");
-
-  if ( $websiteurl != "" ) {
-    $activity_link[0] = "<a href=\"" . $websiteurl . "\" title=\"" . $wl_api_logo_description . "\" target=\"_blank\">";
-    $activity_link[1] = "</a>";
+  if( ! class_exists('ACF') ) {
+    // Do nothing
   } else {
-    $activity_link[0] = "";
-    $activity_link[1] = "";
-  }
 
-  if ( $wl_api_logo_url != "" ){
-    echo "
-      <div class=\"activity-logo\">
-        " . $activity_link[0] . "<img src=\"" . $wl_api_logo_url . "\" title=\"" . $wl_api_logo_description . "\">" . $activity_link[1] . "
-      </div>
-    ";
+    setup_postdata ( $args ) ;
+    
+    // Get website URL 
+    $websiteurl = get_field('websiteurl');
+
+    // Get logo if available
+    $wl_api_logo_description = get_field("logo_description");
+    $wl_api_logo_url = get_field("logo_url");
+
+    if ( $websiteurl != "" ) {
+      $activity_link[0] = "<a href=\"" . $websiteurl . "\" title=\"" . $wl_api_logo_description . "\" target=\"_blank\">";
+      $activity_link[1] = "</a>";
+    } else {
+      $activity_link[0] = "";
+      $activity_link[1] = "";
+    }
+
+    if ( $wl_api_logo_url != "" ){
+      echo "
+        <div class=\"activity-logo\">
+          " . $activity_link[0] . "<img src=\"" . $wl_api_logo_url . "\" title=\"" . $wl_api_logo_description . "\">" . $activity_link[1] . "
+        </div>
+      ";
+    }
   }
-  
 }
 
 add_action( 'init', 'wl_display_activity_logo', 0 );
