a8b17ef6992bfe95c425d2f187a993ea7bf46086
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-05-19 14:09:10 +0100

    type filters

--- global-templates/search-box.php
+++ global-templates/search-box.php
@@ -1,5 +1,5 @@
 <div class="action-box w-100">
 	<h1>What would you like to do?</h1>
 	<?php // get_search_form( 'echo', 'Activity Search');?>
-	<?php echo do_shortcode( '[searchandfilter fields="search,postcodes,costs,activities" types="input, multiselect, multiselect, multiselect" submit_label="Find activities" class="find-activities" search_placeholder="Search for an activity..."]' ); ?>
+	<?php echo do_shortcode( '[searchandfilter fields="search,postcodes,costs,activities" types="multiselect, multiselect, multiselect, multiselect" submit_label="Find activities" class="find-activities" search_placeholder="Search for an activity..."]' ); ?>
 </div>
