b8e367870f889ff194709e49a65e8bd670a99d5c
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-06-29 22:44:16 +0100

    Styling featured on blog landing page

--- loop-templates/content-article-landing.php
+++ loop-templates/content-article-landing.php
@@ -76,12 +76,14 @@ if( have_rows('blog_block') ):
 		$catquery = new WP_Query( $new_query ) ;
 		while($catquery->have_posts()) : $catquery->the_post(); ?>
 			<div class="article__item">
+				<a href="<?php the_permalink() ?>" rel="bookmark">
 				<div class="article__img" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>');">
 				</div>
-				<h3 class="article__title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+				<h3 class="article__title"><?php the_title(); ?></h3>
 				<div class="article__summary">
 					<?php the_excerpt(); ?>
 				</div>
+				</a>
 			</div>
 		<?php endwhile; ?> 
 		<?php wp_reset_postdata(); ?>
