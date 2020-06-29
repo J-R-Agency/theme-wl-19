f98a539b05611b0672534061102aa962ca871c63
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-06-29 15:50:59 +0100

    Remove prefilled search term from widget

--- searchform.php
+++ searchform.php
@@ -13,7 +13,7 @@ defined( 'ABSPATH' ) || exit;
 	<label class="sr-only" for="s"><?php esc_html_e( 'Search', 'understrap' ); ?></label>
 	<div class="input-group">
 		<input class="field form-control" id="s" name="s" type="text"
-			placeholder="<?php esc_attr_e( 'Search &hellip;', 'understrap' ); ?>" value="">
+			placeholder="<?php esc_attr_e( 'New activity search&hellip;', 'understrap' ); ?>" value="">
 		<span class="input-group-append">
 			<input class="submit btn btn-primary" id="searchsubmit" name="submit" type="submit"
 			value="<?php esc_attr_e( 'Search', 'understrap' ); ?>">
