17cb1e52289b860c1e1df464649e54de17aa971c
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-06-29 16:04:57 +0100

    Hide tertiary menu from top nav

--- header.php
+++ header.php
@@ -30,8 +30,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 	
 	<!-- TERTIARY MENU -->
 	<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
-<!-- 
-		<nav class="navbar navbar-expand-md navbar-dark bg-primary" id="tertiary-menu">
+
+		<nav style="display:none;" class="navbar navbar-expand-md navbar-dark bg-primary" id="tertiary-menu">
 			<?php
 				wp_nav_menu(
 					array(
@@ -47,7 +47,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 				);
 			?>
 		</nav>
- -->
+
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 		
