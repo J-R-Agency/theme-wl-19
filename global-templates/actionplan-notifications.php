4665dd117dced18d08cf072c4cd7223e51300c22
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-07-22 16:13:14 +0100

    Extra error checking around Action Plan variables

--- global-templates/actionplan-notifications.php
+++ global-templates/actionplan-notifications.php
@@ -1,7 +1,16 @@
 <?php
 // Display notifications and errors
 
-	echo $frm_email_actionplan__output;
-	echo $frm_share_actionplan__output;
+	if (  isset($frm_email_actionplan__output) ){
 
+			echo $frm_email_actionplan__output;
+
+	}
+
+	if (  isset($frm_share_actionplan__output) ){
+
+			echo $frm_share_actionplan__output;
+			
+	}
+	
 ?>
