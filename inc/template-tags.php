30baf4f7c9d371118de86c3c4a0684b2bca661f4
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-09-25 15:46:27 +0100

    Entry meta

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -17,7 +17,7 @@ if ( ! function_exists( 'understrap_posted_on' ) ) {
 	function understrap_posted_on() {
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s"> (%4$s) </time>';
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
 		}
 		$time_string = sprintf( $time_string,
 			esc_attr( get_the_date( 'c' ) ),
@@ -41,7 +41,7 @@ if ( ! function_exists( 'understrap_posted_on' ) ) {
 				esc_html( get_the_author() )
 			)
 		);
-		echo $posted_on . $byline; // WPCS: XSS OK.
+		echo $posted_on; // WPCS: XSS OK.
 	}
 }
 
